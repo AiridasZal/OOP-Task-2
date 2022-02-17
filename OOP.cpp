094ebc69afc8749ba78ff06dd87f3c12be19fb78
AiridasZal <air.airidas1@gmail.com>
2022-02-17 15:26:01 +0200

    V1.3

--- OOP.cpp
+++ OOP.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
 using std::string;
 
-struct node{
+struct node
+{
     string vardas="", pavarde="";
-    int paz[3]={0}, egz=0;
+    int paz[3]= {0}, egz=0;
     double rezult=0;
     node* next;
 };
 
-void input(node *&root) {
-    int n=3;
+void input(node *&root, int n)
+{
     if (root)
     {
         node *t = root;
@@ -52,20 +54,41 @@ void input(node *&root) {
     }
 }
 
-void galutinis(node *&root) {
+void vidurkis(node *&root, int n)
+{
     double temp=0;
     node *t=root;
-    while(t!=NULL) {
+    while(t!=NULL)
+    {
         temp=0;
-        for(int i=0; i<3; i++) temp+=t->paz[i];
-        t->rezult=0.4*(temp/3)+0.6*t->egz;
+        for(int i=0; i<n; i++) temp+=t->paz[i];
+        t->rezult=0.4*(temp/n)+0.6*t->egz;
         t=t->next;
     }
 }
 
-void output(node *root)
+void median(node *&root, int n)
 {
-    cout << std::setw(20) << std::left << "Vardas" << std::setw(20) << std::left << "Pavarde" << std::setw(5) << std::left << "Galutinis (Vid.)" << std::endl;
+    double temp=0;
+    node *t=root;
+    while(t!=NULL)
+    {
+        temp=0;
+        std::sort(t->paz, t->paz+n);
+        if (n % 2 != 0)
+            temp=(double)t->paz[n/2];
+        else temp=(double)(t->paz[(n-1)/2]+t->paz[n/2])/2.0;
+        t->rezult=0.4*temp+0.6*t->egz;
+        cout << "TEST: " << t->rezult << std::endl;
+        t=t->next;
+    }
+}
+
+void output(node *root, int choice)
+{
+    cout << std::setw(20) << std::left << "Vardas" << std::setw(20) << std::left << "Pavarde";
+    if(choice==0) cout << std::setw(5) << std::left << "Galutinis (Vid.)" << std::endl;
+    if(choice==1) cout << std::setw(5) << std::left << "Galutinis (Med.)" << std::endl;
     cout << "----------------------------------------------------------------------------" << std::endl;
     while(root!=NULL)
     {
@@ -78,13 +101,22 @@ int main()
 {
     node* root = NULL;
     int num;
+    int choice;
+    int n=3;
     cout << "Kiek asmenu skaiciuosit? ";
     cin >> num;
-    for(int i=0; i<num; i++)
-    input(root);
-    galutinis(root);
-    output(root);
+    for(int i=0; i<num; i++) input(root, n);
+OUTPUT:
+    cout << "Galutini bala apaskaiciuoti naudojant namu darbu vidurki ar mediana? (Atitinkamai 0 arba 1): ";
+    cin >> choice;
+    if(choice == 0) vidurkis(root, n);
+    if(choice == 1) median(root, n);
+    if(!((choice>=0)&&(choice<=1)))
+    {
+        goto OUTPUT;
+    }
+    output(root, choice);
     delete[] root;
-//    system("pause");
+    system("pause");
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# OOP-Task-1
+# OOP Program
+## V0.1 Galimybės:
+1. Programa nuskaito vartotojo įvedamus duomenis (struktūros elementus):
+ 
+>*Mokinio vardą, pavardę, namų darbų ir egzamino rezultatus.*
+
+2. Vartotojo įvesti duomenys talpinami į *linked list*.
+3. Programa apskaičiuoja kiekvieno mokinio galutinį balą naudojant formulę:
+
+
+>**_Galutinis_ = 0.4 * _vidurkis_ + 0.6 * _egzaminas_** 
+
+arba 
+
+>**_Galutinis_ = 0.4 * _mediana_ + 0.6 * _egzaminas_**
+
+4. Mokinių kiekis ir įvedamų pažymių skaičius yra dinamiškas dydis.
+5. Galimybė atsitiktinai generuoti mokinio namų darbų ir egzamino balus.
